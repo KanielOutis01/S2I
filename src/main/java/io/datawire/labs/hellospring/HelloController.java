81e45705d532d7584e9487c10cffd53daab9ced9
Nihanth Gandhi <gandhinihanth@gmail.com>
2026-04-17 16:03:48 +0530

    Update HelloController.java

--- src/main/java/io/datawire/labs/hellospring/HelloController.java
+++ src/main/java/io/datawire/labs/hellospring/HelloController.java
@@ -3,7 +3,6 @@ package io.datawire.labs.hellospring;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,7 +17,7 @@ public class HelloController {
                                       TimeUnit.MILLISECONDS.toMinutes(millis),
                                       TimeUnit.MILLISECONDS.toSeconds(millis) -
                                       TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)));
-        return String.format("Hello, nihanth wassappp is god! (up %s, %s)", uptime, System.getenv("BUILD_PROFILE"));
+        return String.format("Hello, nihanth gandhi is god! (up %s, %s)", uptime, System.getenv("BUILD_PROFILE"));
     }
 
 }
